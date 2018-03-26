fixed bad hidden layer backpropagation

src/model/Layers.py
@@ -19,18 +19,17 @@ class HiddenLayer(object):
         self.__hyperparams = hyperparams
 
         # initialize weights and biases
-        # todo weights should be initialized using square root / input size
         self.__weights = np.random.randn(inputSize, outputSize) * 0.01
         self.__biases = np.zeros((1, outputSize)) + 0.01
 
     def forward(self, X):
-        self.__cache = deepcopy(X)
         result = np.dot(X, self.__weights) + self.__biases
+        self.__cache = (deepcopy(X), deepcopy(result))
         return self.__activation.apply(result)
 
     def backprop(self, gradients):
-        X = self.__cache
-        gradientsAct = self.__activation.derivative(X, gradients)
+        X, XActivated = self.__cache
+        gradientsAct = self.__activation.derivative(XActivated, gradients)
         deltaWeights = np.dot(np.transpose(X), gradientsAct)
         deltaBiases = np.sum(gradients, axis=0, keepdims=True)
 